f5adf8635e21f41ee72cc0c6e2658fd8ea1b24a6
DumboKim <rlawlsgus97@gmail.com>
2022-01-27 02:15:23 +0900

    delete firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import {
-  addDoc,
-  collection,
-} from "firebase/firestore";
-import { db } from "../helpers/firebase";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import router from "next/router";
 import styled from "styled-components";
-import { Button, CenterContainer, HeadText, SendButton, SendButtonDis, SmButton, TextArea } from "../components";
+import {
+  Button,
+  CenterContainer,
+  HeadText,
+  SendButton,
+  SendButtonDis,
+  SmButton,
+  TextArea,
+} from "../components";
 import { route } from "next/dist/server/router";
 
 const Home: NextPage = () => {
@@ -23,18 +26,20 @@ const Home: NextPage = () => {
   }, []);
 
   const getUser = async () => {
-    const result = await axios.get('http://localhost/auth', {
-      withCredentials: true
-    }).catch((err) => {
-      router.push('/login');
-    });
+    const result = await axios
+      .get("http://localhost/auth", {
+        withCredentials: true,
+      })
+      .catch((err) => {
+        router.push("/login");
+      });
 
     if (result) {
       if (result.status === 200) {
         setIsLoaded(true);
       }
     }
-  }
+  };
 
   const getTime = async () => {
     let today = new Date();
@@ -49,13 +54,10 @@ const Home: NextPage = () => {
     return todayDate;
   };
 
+  useEffect(() => {}, [frontText]);
 
-  useEffect(() => {
-    
-  }, [frontText]);
-
-  const csvToJson = async (csv_string:string) => {
-    setFrontText('전송중 입니다.');
+  const csvToJson = async (csv_string: string) => {
+    setFrontText("전송중 입니다.");
 
     setBtnState(true);
 
@@ -65,16 +67,29 @@ const Home: NextPage = () => {
 
     const header = rows[0].split(",");
 
-    const headers = header[0].split("\t"); 
-    
-    if (headers[0] !== '날짜' || headers[12] !== '수거 할당') {
+    const headers = header[0].split("\t");
+
+    if (headers[0] !== "날짜" || headers[12] !== "수거 할당") {
       setFrontText(`데이터좀 똑바로 넣으시지!!  날짜부터 수거할당이라고~`);
 
       return;
     }
 
-    const headers2 = ["date", "service", "customer", "item", "region", "building", "lotadrs", "roadadrs", "detailadrs", "password", "feature", "district", "assignee"];
-    
+    const headers2 = [
+      "date",
+      "service",
+      "customer",
+      "item",
+      "region",
+      "building",
+      "lotadrs",
+      "roadadrs",
+      "detailadrs",
+      "password",
+      "feature",
+      "district",
+      "assignee",
+    ];
 
     for (let i = 1; i < rows.length; i++) {
       let obj: {
@@ -83,12 +98,10 @@ const Home: NextPage = () => {
 
       const row = rows[i].split(",");
       console.log(row);
-      
-      
-      const value = row[0].split('\t');
+
+      const value = row[0].split("\t");
       console.log(value);
-      
-      
+
       for (let j = 0; j < headers.length; j++) {
         obj[headers2[j]] = value[j].toString();
       }
@@ -97,9 +110,9 @@ const Home: NextPage = () => {
     }
 
     return jsonArr;
-  }
+  };
 
-  const jsonFileSend = async (jsonFile:any) => {
+  const jsonFileSend = async (jsonFile: any) => {
     const todayDate = await getTime();
 
     console.log(todayDate);
@@ -108,26 +121,24 @@ const Home: NextPage = () => {
 
     const option = {
       method: "post",
-      url: 'http://localhost/collection/assign',
-    }
-    
+      url: "http://localhost/collection/assign",
+    };
 
     for (let i = 0; i < jsonFile.length; i++) {
       try {
-        await axios.post('http://localhost/collection/assign', {
+        await axios.post("http://localhost/collection/assign", {
           ...jsonFile[i],
           // checked: false,
           // index: i + 1,
         });
-
       } catch (e) {
         console.error(e);
-        console.log(i+1 ,'번째에서 에러 발생');
-        
-        setFrontText(`${i+1}번째 데이터에서 에러 발생!`);
+        console.log(i + 1, "번째에서 에러 발생");
+
+        setFrontText(`${i + 1}번째 데이터에서 에러 발생!`);
       }
     }
-    
+
     // for (let i = 0; i < jsonFile.length; i++) {
     //   try {
     //     await addDoc(collection(db, dbTable), {
@@ -139,15 +150,14 @@ const Home: NextPage = () => {
     //   } catch (e) {
     //     console.error(e);
     //     console.log(i+1 ,'번째에서 에러 발생');
-        
+
     //     setFrontText(`${i+1}번째 데이터에서 에러 발생!`);
     //   }
     // }
 
     setBtnState(false);
     setFrontText("전송 완료");
-  }
-
+  };
 
   const addData = async (e: any) => {
     e.preventDefault;
@@ -157,8 +167,8 @@ const Home: NextPage = () => {
 
     if (jsonFile === undefined) {
       setBtnState(false);
-            
-      return ;
+
+      return;
     }
 
     await jsonFileSend(jsonFile);
@@ -167,18 +177,20 @@ const Home: NextPage = () => {
   };
 
   const logout = async () => {
-    const result = await axios.get('http://localhost/auth/logout', {
-      withCredentials: true
-    }).catch((e) => {
-      console.log(e);
-    })
+    const result = await axios
+      .get("http://localhost/auth/logout", {
+        withCredentials: true,
+      })
+      .catch((e) => {
+        console.log(e);
+      });
 
     if (result) {
       if (result.status === 200) {
-        router.push('/login'); 
+        router.push("/login");
       }
-    }    
-  }
+    }
+  };
 
   return (
     <CenterContainer>
@@ -187,30 +199,31 @@ const Home: NextPage = () => {
         <meta name="description" content="Generated by create next app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-        {
-        !isLoaded ? <div>
-        </div> :
-          <>
-            <SmButton onClick={logout}>로그아웃</SmButton>
-            <HeadText>오늘수거 데이터 보내기 v3</HeadText>
-              <TextArea
-                onChange={(e) => {
-                  setTextData(e.target.value);
-                }}
-              />
-              <CenterContainer>
-                {btnState === true ?
-                  <SendButtonDis >데이터 전송</SendButtonDis>:
-                <SendButton onClick={addData} >데이터 전송</SendButton>}
-              <div style={{ textAlign: "center", color: "blue", margin: "15px" }}>
-                {frontText}
-              </div>
-            </CenterContainer>
-          </>
-        }
-      </CenterContainer>
-    
+
+      {!isLoaded ? (
+        <div></div>
+      ) : (
+        <>
+          <SmButton onClick={logout}>로그아웃</SmButton>
+          <HeadText>오늘수거 데이터 보내기 v3</HeadText>
+          <TextArea
+            onChange={(e) => {
+              setTextData(e.target.value);
+            }}
+          />
+          <CenterContainer>
+            {btnState === true ? (
+              <SendButtonDis>데이터 전송</SendButtonDis>
+            ) : (
+              <SendButton onClick={addData}>데이터 전송</SendButton>
+            )}
+            <div style={{ textAlign: "center", color: "blue", margin: "15px" }}>
+              {frontText}
+            </div>
+          </CenterContainer>
+        </>
+      )}
+    </CenterContainer>
   );
 };
 
